eeef81063431bbe731307b51fa9551c34a893e82
Igor Budasov <igor.budasov@persgroep.nl>
2017-10-24 16:49:38 +0200

    removed default bundle

--- src/SleeperBundle/SleeperBundle.php
+++ src/SleeperBundle/SleeperBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace SleeperBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class SleeperBundle extends Bundle
 {
 }
