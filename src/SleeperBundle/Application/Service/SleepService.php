2675cf8ca414468da2cf16975cb97b13e8f63397
Igor Budasov <igor.budasov@gmail.com>
2017-11-16 21:33:08 +0100

    sleep application service

---
+++ src/SleeperBundle/Application/Service/SleepService.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SleeperBundle\Application\Service;
+
+use SleeperBundle\Application\Output\SleepOutput;
+use SleeperBundle\Domain\Exception\SleepByDateNotFoundException;
+use SleeperBundle\Domain\Repository\SleepRepositoryInterface;
+
+class SleepService
+{
+    /** @var SleepRepositoryInterface */
+    private $repository;
+
+    public function __construct(SleepRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param \DateTime $dateTime
+     * @return SleepOutput
+     * @throws SleepByDateNotFoundException
+     */
+    public function getSleepByDate(\DateTime $dateTime): SleepOutput
+    {
+        $sleepModel = $this->repository->getSleepByDate($dateTime);
+
+        return new SleepOutput($sleepModel);
+    }
+
+}
---
+++ tests/SleeperBundle/Application/Service/SleepServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SleeperBundle\Application\Service;
+
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+use SleeperBundle\Application\Output\SleepOutput;
+use SleeperBundle\Domain\Model\Sleep;
+use SleeperBundle\Domain\Repository\SleepRepositoryInterface;
+
+class SleepServiceTest extends TestCase
+{
+    /** @var SleepRepositoryInterface|MockInterface */
+    private $repositoryMock;
+    /** @var Sleep|MockInterface */
+    private $modelMock;
+    /** @var SleepService|MockInterface */
+    private $service;
+
+    protected function setUp()
+    {
+        $this->repositoryMock = \Mockery::mock(SleepRepositoryInterface::class);
+        $this->modelMock = \Mockery::mock(Sleep::class);
+
+        $this->service = new SleepService($this->repositoryMock);
+    }
+
+    public function testThatServiceReturnsMappedOutput()
+    {
+        $startTime = new \DateTime();
+        $endTime = new \DateTime();
+        $deepSleepSeconds = 1;
+        $lightSleepSeconds = 2;
+        $awakeSeconds = 3;
+        $totalSleepSeconds = 4;
+
+        $this->repositoryMock->shouldReceive('getSleepByDate')->once()->andReturn($this->modelMock);
+
+        $this->modelMock->shouldReceive('getStartTime')->once()->andReturn($startTime);
+        $this->modelMock->shouldReceive('getEndTime')->once()->andReturn($endTime);
+        $this->modelMock->shouldReceive('getDeepSleepSeconds')->once()->andReturn($deepSleepSeconds);
+        $this->modelMock->shouldReceive('getLightSleepSeconds')->once()->andReturn($lightSleepSeconds);
+        $this->modelMock->shouldReceive('getAwakeSeconds')->once()->andReturn($awakeSeconds);
+        $this->modelMock->shouldReceive('getTotalSleepSeconds')->once()->andReturn($totalSleepSeconds);
+
+        $output = $this->service->getSleepByDate(new \DateTime());
+
+        self::assertInstanceOf(SleepOutput::class, $output);
+        self::assertEquals($startTime, $output->getStartTime());
+        self::assertEquals($endTime, $output->getEndTime());
+        self::assertEquals($deepSleepSeconds, $output->getDeepSleepSeconds());
+        self::assertEquals($lightSleepSeconds, $output->getLightSleepSeconds());
+        self::assertEquals($awakeSeconds, $output->getAwakeSeconds());
+        self::assertEquals($totalSleepSeconds, $output->getTotalSleepSeconds());
+    }
+}
