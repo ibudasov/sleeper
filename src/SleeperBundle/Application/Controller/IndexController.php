04f92a4ae9be756f1692c77d2f6f9a13a1cd0ac4
Igor Budasov <igor.budasov@gmail.com>
2017-12-13 18:10:30 +0100

    dummy response for /

---
+++ src/SleeperBundle/Application/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SleeperBundle\Application\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController
+{
+    /**
+     * @Route("/")
+     * @return JsonResponse
+     */
+    public function indexAction(): JsonResponse
+    {
+        return new JsonResponse([
+            'Sleeper API dummy response.',
+            'For actual response -- look documentation: https://github.com/ibudasov/sleeper'
+        ]);
+    }
+}
---
+++ tests/SleeperBundle/Application/Controller/IndexControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use SleeperBundle\Application\Controller\IndexController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class IndexControllerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testThatDummyResponseIsReturnedWhenNoPathIsSpecified(): void
+    {
+        $controller = new IndexController();
+        $response = $controller->indexAction();
+
+        self::assertInstanceOf(JsonResponse::class, $response);
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals(
+            \json_encode([
+                'Sleeper API dummy response.',
+                'For actual response -- look documentation: https://github.com/ibudasov/sleeper'
+            ]),
+            $response->getContent()
+        );
+    }
+}
+
