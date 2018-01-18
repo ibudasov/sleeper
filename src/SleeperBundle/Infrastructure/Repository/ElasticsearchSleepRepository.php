8ec5459ea348d5e1384533997dff13ad820f07c2
Igor Budasov <igor.budasov@gmail.com>
2018-01-18 14:37:45 +0100

    usage of http-client in elasticsearch repo

--- src/SleeperBundle/Infrastructure/Repository/ElasticsearchSleepRepository.php
+++ src/SleeperBundle/Infrastructure/Repository/ElasticsearchSleepRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SleeperBundle\Infrastructure\Repository;
 
+use GuzzleHttp\Client;
 use SleeperBundle\Domain\Entity\Sleep;
 use SleeperBundle\Domain\Repository\SleepRepositoryInterface;
 use SleeperBundle\Domain\ValueObject\IdentityInterface;
@@ -11,16 +12,32 @@ use SleeperBundle\Domain\ValueObject\SleepId;
 
 class ElasticsearchSleepRepository implements SleepRepositoryInterface
 {
+    private const ELASTICSEARCH_BASE_PATH = 'localhost:9200/sleep/night';
 
-    /** @inheritdoc */
+    /**
+     * @var Client
+     */
+    private $httpClient;
+
+    /**
+     * @param Client $httpClient
+     */
+    public function __construct(Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /** {@inheritdoc} */
     public function generateId(): IdentityInterface
     {
         return SleepId::generate();
     }
 
-    /** @inheritdoc */
+    /** {@inheritdoc} */
     public function getSleepByDate(\DateTime $date): Sleep
     {
+        $this->httpClient->get(self::ELASTICSEARCH_BASE_PATH);
+
         return new Sleep(
             $this->generateId(),
             new \DateTime(),
--- tests/SleeperBundle/Infrastructure/ElasticsearchSleepRepositoryTest.php
+++ tests/SleeperBundle/Infrastructure/ElasticsearchSleepRepositoryTest.php
@@ -2,16 +2,31 @@
 
 declare(strict_types=1);
 
+use GuzzleHttp\Client;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
+use SleeperBundle\Domain\Entity\Sleep;
 use SleeperBundle\Infrastructure\Repository\ElasticsearchSleepRepository;
 
 class ElasticsearchSleepRepositoryTest extends TestCase
 {
-    public function testThatSleepCanBeRetrievedByDate(): void
-    {
-        $repo = new ElasticsearchSleepRepository();
+    /** @var Client|MockInterface */
+    private $httpClientMock;
 
-        self::assertInstanceOf(\SleeperBundle\Domain\Entity\Sleep::class, $repo->getSleepByDate(new \DateTime()));
+    protected function setUp(): void
+    {
+        $this->httpClientMock = Mockery::mock(Client::class);
     }
 
+    public function testThatSleepCanBeRetrievedByDate(): void
+    {
+        $repo = new ElasticsearchSleepRepository($this->httpClientMock);
+
+        $this->httpClientMock->shouldReceive('get')
+            ->once()
+            ->with('localhost:9200/sleep/night')
+            ->andReturn('1111');
+
+        self::assertInstanceOf(Sleep::class, $repo->getSleepByDate(new \DateTime()));
+    }
 }
